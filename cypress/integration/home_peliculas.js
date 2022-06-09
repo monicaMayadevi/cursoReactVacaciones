2479e7557869ccfa9ba28034857b0bf334f35e7d
Monica Silva <msilva@correo.ler.uam.mx>
2022-06-09 12:04:45 -0500

    cypress-removefile4

--- cypress/integration/home_peliculas.js
+++ cypress/integration/home_peliculas.js
@@ -6,17 +6,7 @@ describe('home peliculas', () =>
   })
   it('listar enlances a los listados de peliculas', () =>
   {
-    cy.get('h2')
-    .should('contain', 'Página principal de peliculas')
     cy.get('a').contains('Peliculas Infantiles').click()
     cy.url().should('include', '/peliculas/A')
-    cy.get('a').contains('Peliculas Adolescentes').click()
-    cy.url().should('include', '/peliculas/B')
-    cy.get('a').contains('Peliculas Adultos').click()
-    cy.url().should('include', '/peliculas/C')
-    cy.get('a').contains('Mostrar Variables').click()
-    cy.url().should('include', '/mostrarDatos')
-    cy.get('a').contains('Home').click()
-    cy.url().should('include', '/')
   })
   })
